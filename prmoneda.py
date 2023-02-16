12d54122a5de396f29a04a13a335c9acefba5e49
maitegomezsanchez <125576934+maitegomezsanchez@users.noreply.github.com>
2023-02-16 16:43:48 +0000

    act

---
+++ prmoneda.py
@@ -0,0 +1,11 @@
+import requests
+#currency = "eur"
+#basecurrency = "aud"
+#url = "http://freecurrencyrates.com/api/action.php?do=cvals&iso=" + currency + "&f=" + basecurrency + "&v=1&s=cbr"
+#resp =requests.get(url)
+#print(resp.text)
+#print(resp.json())
+url = "http://jsonplaceholder.typicode.com/posts"
+resp =requests.get(url)
+print(resp.text)
+print(resp.json())
