5ba845853fca3652347abd5a70746072280ed9c0
maitegomezsanchez <125576934+maitegomezsanchez@users.noreply.github.com>
2023-02-23 15:52:18 +0000

    sal

--- jueves.py
+++ jueves.py
@@ -1,8 +1,14 @@
 import funciones
+import requests
 if __name__=='__main__':
     num1=float(input("inserta 1nº para realizar resta: "))
     num2=float(input("inserta 2nº para realizar resta: "))
     if funciones.resta(num1,num2)>10:
-        print(f"la resta {resta(num1,num2)} es mayor q 10")
+       print(f"la resta {funciones.resta(num1,num2)} es mayor q 10")
     else: 
-        print(f"la resta {resta(num1,num2)} es menor o igual a 10")
+       print(f"la resta {funciones.resta(num1,num2)} es menor o igual a 10")
+  
+    link = "htpp://info.cern.ch/"
+    r=requests.get(link)
+    print(r.status_code) #si el resultado es 200 es pq ha encontrado la pagina
+    html=r.text
